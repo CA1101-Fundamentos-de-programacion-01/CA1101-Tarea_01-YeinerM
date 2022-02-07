ecb24a007fea1816ce0ec14a2c92d7c2a8bc1a7d
YeinerM <99045921+YeinerM@users.noreply.github.com>
2022-02-07 12:47:45 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -7,10 +7,10 @@ y = 3
 
 resultado  =  z  +  x  *  365  -  y  *  52
 
-z=monedas_encontradas
+monedas_encontradas=z
 
-x=monedas_robada
+monedas_robadas=x
 
-y=monedas_magicas
+monedas_magicas=y
 
 resultado=monedas_encontradas+monedas_robadas*365-monedas_magicas*52
