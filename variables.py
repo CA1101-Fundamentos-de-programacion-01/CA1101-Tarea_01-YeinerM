59908d694d7d8d2260966b6b78db4b46d65ab019
YeinerM <99045921+YeinerM@users.noreply.github.com>
2022-02-07 11:06:11 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,4 +5,13 @@ x = 10
 y = 3
 
 
-resultado = z + x * 365 - y * 52
+resultado  =  z  +  x  *  365  -  y  *  52
+
+z=monedas_encontradas
+
+x=monedas_robada
+
+y=monedas_magicas
+
+
+resultado=monedas_encontradas+monedas_robadas*365-monedas_magicas*52
