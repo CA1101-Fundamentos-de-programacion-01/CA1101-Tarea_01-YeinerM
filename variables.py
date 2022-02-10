a83250137c10af2a34704b695e48afe04049a466
YeinerM <99045921+YeinerM@users.noreply.github.com>
2022-02-09 21:03:50 -0600

    Revert "Feedback"

--- variables.py
+++ variables.py
@@ -5,12 +5,4 @@ x = 10
 y = 3
 
 
-resultado  =  z  +  x  *  365  -  y  *  52
-
-monedas_encontradas=z
-
-monedas_robadas=x
-
-monedas_magicas=y
-
-resultado=monedas_encontradas+monedas_robadas*365-monedas_magicas*52
+resultado = z + x * 365 - y * 52
